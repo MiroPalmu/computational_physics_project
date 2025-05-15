3c657adcab8d84df92e19326c83bd222a372e994
Miro Palmu <email@miropalmu.cc>
2025-05-15 14:54:16 +0300

    fixup! calculate hamiltonian constraint

--- w2_bssn_uniform_grid.cpp
+++ w2_bssn_uniform_grid.cpp
@@ -848,14 +848,15 @@ w2_bssn_uniform_grid::pre_calculations(const real km) const {
             u8"ij,ij"_einsum((*AA_ptr)[idx], (*contraconf_A_ptr)[idx], coconf_A_[idx]);
         });
 
-        constraints_ptr->hamiltonian.for_each_index(
-            [SPTR(R_ptr, AA_ptr, constraints_ptr), this](const auto idx) {
-                const auto a                        = (*R_ptr)[idx][] * W_[idx][] * W_[idx][];
-                const auto b                        = real{ 2 } * K_[idx][] * K_[idx][] / real{ 3 };
-                const auto c                        = (*AA_ptr)[idx][];
-                constraints_ptr->hamiltonian[idx][] = a + b - c;
-            });
+        auto H_ptr = allocate_buffer<0>(grid_size_);
+        H_ptr->for_each_index([SPTR(H_ptr, R_ptr, AA_ptr), this](const auto idx) {
+            const auto a    = (*R_ptr)[idx][] * W_[idx][] * W_[idx][];
+            const auto b    = real{ 2 } * K_[idx][] * K_[idx][] / real{ 3 };
+            const auto c    = (*AA_ptr)[idx][];
+            (*H_ptr)[idx][] = a + b - c;
+        });
         tensor_buffer_queue.wait();
+        constraints_ptr->hamiltonian = std::move(*H_ptr);
     }
 
     { // momentum constraint damping
